3db3f391261c91450f3445cc9940f3f929bf1606
Mikkel Ricky <rimi@aarhus.dk>
2024-12-03 13:14:06 +0100

    3159: More roles stuff

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -61,10 +61,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        $roles[] = 'ROLE_ADMIN';
-
-        return array_unique($roles);
+        return $this->roles;
     }
 
     public function setRoles(array $roles): self
