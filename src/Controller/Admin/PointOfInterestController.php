ef629507418e940d07c6872e9a7f9fee3849cfa3
Sine Jespersen <sinejespersen@gmail.com>
2024-12-03 10:50:00 +0100

    lint

--- src/Controller/Admin/PointOfInterestController.php
+++ src/Controller/Admin/PointOfInterestController.php
@@ -7,14 +7,14 @@ use App\Entity\Role;
 use App\Field\VichFileField;
 use App\Field\VichImageField;
 use App\Service\EasyAdminHelper;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use Symfony\Component\Translation\TranslatableMessage;
 
 class PointOfInterestController extends AbstractCrudController
